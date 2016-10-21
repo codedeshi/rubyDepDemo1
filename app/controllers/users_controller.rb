cbca2063f65678990e6c0b8d7b33f960e5c6573e
codedeshi <singh.hardit@gmail.com>
2016-10-20 22:56:08 -0500

    test reglog

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
+	# before_action :require_login, except: [:new, :create]
+	# before_action :require_correct_user, only: [:show, :edit, :update, :destroy]
 
-	def index
+	def create
+		user = User.create(user_params)
+		if user.errors.full_messages.length !=0 
+			flash[:errors] = user.errors.full_messages 
+			redirect_to new_user_path
+		else 
+			session[:user_id] = user.id
+			redirect_to user_path(user.id)
+		end
+	end
+
+	private
+
+	def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
 	end
 end
+
