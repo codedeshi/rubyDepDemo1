42e785ed8524699faeb37bea186850973ab654d1
codedeshi <singh.hardit@gmail.com>
2016-10-24 10:35:55 -0500

    reglog

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+		def create
+		user = User.find_by(:email => user_params[:email])
+		if user
+			if  user.authenticate(user_params[:password])
+				session[:user_id] = user.id 
+				redirect_to user_path(session[:user_id])
+			else
+				flash[:errors] = ["Invalid password"]
+				redirect_to :back
+			end
+		else
+			flash[:errors] = ["User not found"]
+			redirect_to :back
+		end 
+	end 
+
+	def destroy
+		session.clear
+		redirect_to "/main"
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+	def create
+		user = User.create(user_params)
+		if user.errors.full_messages.length !=0 
+			flash[:errors] = user.errors.full_messages 
+			redirect_to :back
+		else 
+			session[:user_id] = user.id
+			redirect_to "/dashboard/#{user.id}"
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(:fname, :lname,:email, :password)
+	end
+end
